f8a8e5f74fc1611f262fb2f4b8d9870b2e738033
Gaurav Kumar <gauravk2398@gmail.com>
2026-05-26 23:51:41 +0530

    refactor: minor config change

--- salary-api/config/environments/test.rb
+++ salary-api/config/environments/test.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
+  # Allow RSpec mock requests
+  config.hosts << "www.example.com"
   # Allow any host in test mode
   config.hosts << /.*/
 end
